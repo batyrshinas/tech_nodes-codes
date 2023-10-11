f66adffedcdfeb7686d33bda8833b4196b3ffd29
batyrshinas <82641666+batyrshinas@users.noreply.github.com>
2023-10-11 15:46:06 +0500

    Add files via upload

---
+++ cascadia-upgrade3.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+tput reset
+tput civis
+
+echo "  "
+echo "  "
+
+echo "ОСТАНАВЛИВАЕМ СЕРВИС"
+echo "  "
+echo "  "
+sudo systemctl stop cascadiad
+
+echo "  "
+echo "  "
+echo "ОБНОВЛЕНИЕ БИНАРНИКА"
+echo "  "
+echo "  "
+rm /usr/local/bin/cascadiad
+cd $HOME
+curl -L https://github.com/CascadiaFoundation/cascadia/releases/download/v0.1.6/cascadiad -o cascadiad
+cd bin/
+sudo chmod u+x cascadiad
+sudo cp cascadiad /usr/local/bin/cascadiad
+sudo chown $USER /usr/local/bin/cascadiad
+cd $HOME
+rm -r bin
+echo "  "
+echo "  "
+echo "СТАРТ СЕРВИСА"
+echo "  "
+echo "  "
+sudo systemctl restart cascadiad
+echo "  "
+echo "  "
+echo "СТАТУС СЕРВИСА"
+echo "  "
+echo "  "
+systemctl status cascadiad
+exit
+
