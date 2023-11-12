3dccb502e463cfd9908c4942e740554b59d2cf49
batyrshinas <82641666+batyrshinas@users.noreply.github.com>
2023-11-12 13:19:26 +0500

    Update shardeum.sh

--- shardeum.sh
+++ shardeum.sh
@@ -11,7 +11,7 @@ sudo apt-get install curl -y
 sudo apt update
 sudo apt install docker.io -y
 docker --version
-sudo curl -L "https://github.com/docker/compose/releases/download/2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo docker-compose --version
 sudo apt install make clang git pkg-config libssl-dev build-essential git gcc chrony curl jq ncdu bsdmainutils htop net-tools lsof fail2ban wget -y
