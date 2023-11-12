0a30f524e98dfceae2aa1ed07b2b80b5808e91d1
batyrshinas <82641666+batyrshinas@users.noreply.github.com>
2023-11-12 11:51:58 +0500

    Update shardeum-update-validator.sh
    + ip address

--- shardeum-update-validator.sh
+++ shardeum-update-validator.sh
@@ -4,7 +4,9 @@ tput civis
 
 
 echo " "
+echo " "
 echo "ОБНОВЛЕНИЕ НОДЫ VALIDATOR SHARDEUM"
+echo " "
 echo " "
 
 curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh
@@ -17,3 +19,10 @@ operator-cli start
 operator-cli status
 exit
 
+your_server_ip=$(wget -qO- eth0.me)
+echo " "
+echo " "
+echo "******************* ОбНОВЛЕНИЕ НОДЫ ЗАВЕРШЕНА *******************"
+echo " "
+echo " "
+echo "Для проверки ноды перейдите по ссылке https://$your_server_ip:8080"
