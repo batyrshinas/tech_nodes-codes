40a73b8fea61087ae557d414acb644d715bb181a
batyrshinas <82641666+batyrshinas@users.noreply.github.com>
2023-11-12 11:49:46 +0500

    Update shardeum-update-validator.sh
    + operator-cli

--- shardeum-update-validator.sh
+++ shardeum-update-validator.sh
@@ -9,4 +9,11 @@ echo " "
 
 curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh
 
+cd $HOME
+cd .shardeum
+./shell.sh
 operator-cli gui start
+operator-cli start
+operator-cli status
+exit
+
