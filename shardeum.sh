b34d0d061e87eb2cfcd2fdf56d1bd2f179edf48b
batyrshinas <82641666+batyrshinas@users.noreply.github.com>
2023-11-12 12:17:30 +0500

    Update shardeum.sh

--- shardeum.sh
+++ shardeum.sh
@@ -3,7 +3,9 @@ tput reset
 tput civis 
 your_server_ip=$(wget -qO- eth0.me)
 echo " "
+echo " "
 echo "УСТАНОВКА ПРЕРЕКВИЗИТОВ"
+echo " "
 echo " "
 sudo apt-get install curl -y
 sudo apt update
@@ -11,20 +13,23 @@ sudo apt install docker.io -y
 docker --version
 sudo curl -L "https://github.com/docker/compose/releases/download/2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+sudo docker-compose --version
+sudo apt install make clang git pkg-config libssl-dev build-essential git gcc chrony curl jq ncdu bsdmainutils htop net-tools lsof fail2ban wget -y
 
+echo " "
 echo " "
 echo "НАЧИНАЕМ УСТАНОВКУ НОДЫ SHARDEUM"
 echo " "
+echo " "
 
 cd $HOME
-
 sudo apt update && sudo apt upgrade -y
-sudo apt install make clang git pkg-config libssl-dev build-essential git gcc chrony curl jq ncdu bsdmainutils htop net-tools lsof fail2ban wget -y
-curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh
+sudo curl -O "https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh" && chmod +x installer.sh && ./installer.sh
 
 echo " "
+echo " "
 echo "ЗАПУСК ВАДИДАТОРА SHARDEUM"
+echo " "
 echo " "
 cd $HOME
 cd .shardeum
@@ -36,4 +41,4 @@ exit
 cd $HOME
 
 echo "******************* УСТАНОВКА НОДЫ ЗАВЕРШЕНА *******************"
-echo "Для запуска ноды перейдите по ссылке https://$your_server_ip:8080"
+echo "Для запуска ноды перейдите на дашборд по ссылке https://$your_server_ip:8080"
