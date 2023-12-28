d29494f40a28b2d1727fc2d5ea0cc85cbe4e0eae
batyrshinas <82641666+batyrshinas@users.noreply.github.com>
2023-12-28 22:58:59 +0500

    Update taiko-node.sh

--- taiko-node.sh
+++ taiko-node.sh
@@ -70,35 +70,35 @@ echo " "
 echo " "
 
 # Install docker
-echo "УСТАНОВКА DOCKER..."
-sudo apt-get install ca-certificates curl gnupg lsb-release
+#echo "УСТАНОВКА DOCKER..."
+#sudo apt-get install ca-certificates curl gnupg lsb-release
 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+#sudo mkdir -p /etc/apt/keyrings
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-echo \
-"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#echo \
+#"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-sudo apt-get update
+#sudo chmod a+r /etc/apt/keyrings/docker.gpg
+#sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-sudo apt install docker-compose
+#sudo apt install docker-compose
 
 #inslall need packets for docker
-sudo snap install docker -y
-sudo apt  install podman-docker -y
-sudo apt  install docker.io -y
-sudo service docker start
-sudo apt-get update -y
-echo " "
-echo " "
-echo "ПРОВЕРКА РАБОТОСПОСОБНОСТИ DOCKER..."
-echo " "
-echo " "
-sudo docker run hello-world
+#sudo snap install docker -y
+#sudo apt  install podman-docker -y
+#sudo apt  install docker.io -y
+#sudo service docker start
+#sudo apt-get update -y
+#echo " "
+#echo " "
+#echo "ПРОВЕРКА РАБОТОСПОСОБНОСТИ DOCKER..."
+#echo " "
+#echo " "
+#sudo docker run hello-world
 echo " "
 echo " "
 echo "НЕОБХОДИМЫЕ ПАКЕТЫ УСТАНОВЛЕНЫ"
