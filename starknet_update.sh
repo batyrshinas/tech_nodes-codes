ccf77eefae078363fec71c8dc2e500975c2d51e5
batyrshinas <82641666+batyrshinas@users.noreply.github.com>
2024-01-10 16:48:13 +0500

    update to 0.10.3 and change to Sepolia net

--- starknet_update.sh
+++ starknet_update.sh
@@ -11,6 +11,12 @@ fi
 #curl -s https://api.nodes.guru/logo.sh | bash
 #echo "==================================================="
 sleep 2
+
+read -p "ВВЕДИТЕ ВАШ ALCHEMY API KEY (МОЖНО НАЙТИ НАЖАВ на VIEW KEY НА САЙТЕ https://dashboard.alchemy.com/apps): " API && sleep 2
+ALCHEMY=https://eth-sepolia.g.alchemy.com/v2/$API
+echo 'export ALCHEMY='$ALCHEMY >> $HOME/.bash_profile
+
+
 sudo apt update && sudo apt install pkg-config libssl-dev libzstd-dev protobuf-compiler -y
 #sudo add-apt-repository ppa:deadsnakes/ppa -y
 #sudo apt update && sudo apt install curl git tmux python3.10 python3.10-venv python3.10-dev build-essential libgmp-dev pkg-config libssl-dev -y
@@ -22,7 +28,7 @@ rustup update stable --force
 cd ~/pathfinder
 git pull
 git fetch --all
-git checkout v0.10.2
+git checkout v0.10.3
 cargo build --release --bin pathfinder
 sudo mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
 mkdir -p $HOME/.starknet/db
