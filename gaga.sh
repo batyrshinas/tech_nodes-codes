b231fb94e04ad01ce40cd3f6d211bc137bf86c9c
batyrshinas <82641666+batyrshinas@users.noreply.github.com>
2024-01-18 23:53:25 +0500

    Update gaga.sh

--- gaga.sh
+++ gaga.sh
@@ -41,8 +41,13 @@ echo " "
 echo " "
 cd $HOME
 sudo apt-get update -y && sudo apt-get -y install curl tar ca-certificates
-curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-amd64.tar.gz && rm -f apphub-linux-amd64.tar.gz && cd ./apphub-linux-amd64 && sudo ./apphub service install
-sleep 10
+curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz 
+tar -zxf apphub-linux-amd64.tar.gz 
+rm -f apphub-linux-amd64.tar.gz 
+cd ./apphub-linux-amd64 
+sudo ./apphub service remove 
+sudo ./apphub service install
+sleep 20
 echo ""
 echo "ЗАПУСКАЕМ НОДУ"
 sudo ./apphub service start
