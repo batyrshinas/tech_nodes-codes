f71f9a9f12389a4eb049403be168ae3ab90691b4
batyrshinas <82641666+batyrshinas@users.noreply.github.com>
2024-03-31 12:09:19 +0500

    Update cascadia-delete.sh

--- cascadia-delete.sh
+++ cascadia-delete.sh
@@ -5,22 +5,22 @@ tput civis
 echo " "
 echo " "
 
-echo "ПОДТВЕРЖДАЕТЕ УДАЛЕНИЕ НОДЫ CASCADIA ИЗ ВАШЕГО СЕРВЕРА?"
-echo " "
-echo "ЕСЛИ ДА, ТО ВВЕДИТЕ В АНГЛ.РАСКЛАДКЕ БУКВУ «y»"
-echo "ЕСЛИ НЕТ, ТО ВВЕДИТЕ В АНГЛ.РАСКЛАДКЕ БУКВУ «n»"
-echo "ВВЕДИТЕ ОТВЕТ:"
-read item
-case "$item" in
-    y|Y) echo "ВЫ ВВЕЛИ «y», НАЧИНАЕМ ПРОЦЕСС УДАЛЕНИЯ..."
-        ;;
-    n|N) echo "ВЫ ВВЕЛИ «n», ПРОЦЕСС УДАЛЕНИЯ ПРЕКРАЩЕН"
-        exit 0
-        ;;
-    *) echo "ВЫ НИЧЕГО НЕ ВВЕЛИ. ВЫХОД..."
-         exit 1
-        ;;
-esac
+#echo "ПОДТВЕРЖДАЕТЕ УДАЛЕНИЕ НОДЫ CASCADIA ИЗ ВАШЕГО СЕРВЕРА?"
+#echo " "
+#echo "ЕСЛИ ДА, ТО ВВЕДИТЕ В АНГЛ.РАСКЛАДКЕ БУКВУ «y»"
+#echo "ЕСЛИ НЕТ, ТО ВВЕДИТЕ В АНГЛ.РАСКЛАДКЕ БУКВУ «n»"
+#echo "ВВЕДИТЕ ОТВЕТ:"
+#read item
+#case "$item" in
+#    y|Y) echo "ВЫ ВВЕЛИ «y», НАЧИНАЕМ ПРОЦЕСС УДАЛЕНИЯ..."
+#        ;;
+#    n|N) echo "ВЫ ВВЕЛИ «n», ПРОЦЕСС УДАЛЕНИЯ ПРЕКРАЩЕН"
+#        exit 0
+#        ;;
+#    *) echo "ВЫ НИЧЕГО НЕ ВВЕЛИ. ВЫХОД..."
+#         exit 1
+#        ;;
+#esac
 
 
 echo " "
@@ -35,6 +35,7 @@ sudo rm -rf $HOME/cascadia
 sudo rm -rf /etc/systemd/system/cascadiad.service
 sudo rm -rf /usr/local/bin/cascadiad
 sudo systemctl daemon-reload
+sudo rm //usr/local/bin/cascadia_auto_delegate.sh
 
 echo " "
 echo " "
@@ -44,4 +45,3 @@ echo " "
 exit
 echo " "
 echo " "
-
