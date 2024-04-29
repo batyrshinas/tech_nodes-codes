d19ebf16c4eb25e9b90cf9f1e2436fae36bf49b8
batyr2405 <101793307+batyr2405@users.noreply.github.com>
2024-04-29 13:50:36 +0300

    Update subspace_docker-upgrade.sh

--- subspace_docker-upgrade.sh
+++ subspace_docker-upgrade.sh
@@ -24,9 +24,9 @@ echo " "
 echo " "
 
 
-OLD_IMAGE_NODE="node:gemini-3h-2024-apr-26"
+OLD_IMAGE_NODE="node:gemini-3h-2024-apr-24"
 NEW_IMAGE_NODE="node:gemini-3h-2024-apr-26-2"
-OLD_IMAGE_FARMER="farmer:gemini-3h-2024-apr-26"
+OLD_IMAGE_FARMER="farmer:gemini-3h-2024-apr-24"
 NEW_IMAGE_FARMER="farmer:gemini-3h-2024-apr-26-2"
 DOCKER_COMPOSE_FILE="docker-compose.yaml"
 
