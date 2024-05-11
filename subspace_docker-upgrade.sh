62f9e4ed57d4b1ca54dd86a6f129d4505126bdff
batyr2405 <101793307+batyr2405@users.noreply.github.com>
2024-05-11 14:42:22 +0500

    Update subspace_docker-upgrade.sh

--- subspace_docker-upgrade.sh
+++ subspace_docker-upgrade.sh
@@ -24,10 +24,10 @@ echo " "
 echo " "
 
 
-OLD_IMAGE_NODE="node:gemini-3h-2024-apr-26-2"
-NEW_IMAGE_NODE="node:gemini-3h-2024-may-01"
-OLD_IMAGE_FARMER="farmer:gemini-3h-2024-apr-26-2"
-NEW_IMAGE_FARMER="farmer:gemini-3h-2024-may-01"
+OLD_IMAGE_NODE="node:gemini-3h-2024-may-01"
+NEW_IMAGE_NODE="node:gemini-3h-2024-may-06"
+OLD_IMAGE_FARMER="farmer:gemini-3h-2024-may-01"
+NEW_IMAGE_FARMER="farmer:gemini-3h-2024-may-06"
 DOCKER_COMPOSE_FILE="docker-compose.yaml"
 
 # Проверяем, существует ли файл docker-compose.yaml
